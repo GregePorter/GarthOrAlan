af35a6c22b81099bd4c210fb1bbd3110c3cb0eec
Gregory Porter <greg.e.porter@gmail.com>
2024-10-09 21:42:28 -0500

    Added fly coonfiguration

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,12 +11,12 @@ const images = [
   {
     url: garth,
     title: 'Garth',
-    width: '30%',
+    width: '50%',
   },
   {
     url: alan,
     title: 'Alan',
-    width: '30%',
+    width: '50%',
   },
 ];
 
@@ -87,14 +87,22 @@ const ImageMarked = styled('span')(({ theme }) => ({
 const getNextQuote = () => {
   let quotes = [
     { line : 'You know, as a writer, if you take away my paper, I would write on my heart.', name : 'Garth'},
+    { line : 'As a horror writer I don\'t ask for much. I just hope I\'ve changed the way you think about life.', name : 'Garth'},
     { line : 'My books are all essentially about ‘what ifs’.', name : 'Garth'},
+    { line : 'I\'ve always loved the great tragedies, King Lear, The Poseidon Adventure, Superman 2.', name : 'Garth'},
+    { line : 'As a writer, I make my own rules up okay. If I wanna start a sentence with a full-stop I will.', name : 'Garth'},
     { line : 'The unanswered mystery is what stays with us the longest, and it’s what we’ll remember in the end.', name : 'Alan'},
-    { line : 'Steven King once wrote that nightmares exist outside of logic and there’s little fun to be had in explanations. They’re antithetical to the poetry of fear. In a horror story the victim keeps asking why, but there can be no explanation and there shouldn’t be one.', name : 'Alan'},
+    { line : 'Steven King once wrote that nightmares exist outside of logic and there’s little fun to be had in explanations. They’re antithetical to the poetry of fear.', name : 'Alan'},
+    { line : 'In a horror story the victim keeps asking why, but there can be no explanation and there shouldn’t be one.', name : 'Alan'},
     { line : 'It\'s not a lake, it\'s an ocean.', name : 'Alan'},
-    { line : 'The shriek sounded again. No way was it a human cry. No human vocal cord could produce such a bowel-shatteringly terrifying cacophony as that.', name : 'Garth'}
+    { line : 'The shriek sounded again. No way was it a human cry. No human vocal cord could produce such a bowel-shatteringly terrifying cacophony as that.', name : 'Garth'},
+    { line : 'What... What\'re the Christmas lights for?', name : 'Alan'},
+    { line : 'I’ve always had a vivid imagination, but this dream unsettled me. It was wild and dark and weird, even by my standards.', name : 'Alan'},
+    { line : 'A story is not a machine that does what you tell it.', name : 'Alan'},
+    { line : 'A story is a beast with a life of its own.', name : 'Alan'},
   ]
 
-  let toReturn = quotes[Math.floor(Math.random() * (6 - 1) + 1)];
+  let toReturn = quotes[Math.floor(Math.random() * (14 - 1) + 1)];
   return [ toReturn['line'], toReturn.name]
 }
 
@@ -126,11 +134,13 @@ export default function ButtonBaseDemo() {
 
   return (
     <>
-    <p>{quote[0]}</p>
-    { !onNewRound && <Box >
+      <Box sx={{ padding: '2em'}} >
+        <h1>{quote[0]}</h1>
+      </Box>
+    { !onNewRound && <Box sx={{ padding: '2em'}} >
       { correctGuess && <h1>Nice job! {quote[1]} said that one</h1>}
       { !correctGuess && <h1>Try again! {quote[1]} said that one</h1>}
-      <Button onClick={handleNextRound}>New Round! </Button>
+      <Button variant="outlined" onClick={handleNextRound}>New Round!</Button>
       </Box>
     }
     <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
